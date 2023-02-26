c690e139e3984e33fd59d544eebd6e3d80a526da
znwhyzzq <2514765163@qq.com>
2023-02-26 14:41:50 +0800

    Initial commit

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/test/java/com/exuecheng/XuechengPlusContentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exuecheng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengPlusContentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
